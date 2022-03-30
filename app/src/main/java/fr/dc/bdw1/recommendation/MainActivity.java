7d7e7de78dedaaf60cd483f506a9c24e4b97866b
4ndr01 <31137819+4ndr01@users.noreply.github.com>
2022-03-30 16:41:20 +0200

    push2

--- app/src/main/java/fr/dc/bdw1/recommendation/MainActivity.java
+++ app/src/main/java/fr/dc/bdw1/recommendation/MainActivity.java
@@ -3,6 +3,9 @@ package fr.dc.bdw1.recommendation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CheckBox c1 = this.findViewById(R.id.checkBox);
+        CheckBox c2 = this.findViewById(R.id.checkBox2);
+        CheckBox c3 = this.findViewById(R.id.checkBox3);
+
+
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
