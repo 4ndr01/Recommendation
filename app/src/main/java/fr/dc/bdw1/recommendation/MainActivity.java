c4e11f0e94b5b59feb426f6ceb36bf684111985f
4ndr01 <31137819+4ndr01@users.noreply.github.com>
2022-04-06 11:55:54 +0200

    push version1

--- app/src/main/java/fr/dc/bdw1/recommendation/MainActivity.java
+++ app/src/main/java/fr/dc/bdw1/recommendation/MainActivity.java
@@ -16,13 +16,35 @@ import fr.dc.bdw1.recommendation.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     public static final String url = "URL";
     private ActivityMainBinding ui;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        ui= ActivityMainBinding.inflate(getLayoutInflater());
+        ui = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(ui.getRoot());
 
+        ActivityResultLauncher<Intent> accessoireActivityResultLauncher = this.registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        switch (result.getResultCode()) {
+                            case RESULT_CANCELED:
+                                break;
+
+                            case RESULT_OK: {
+
+
+                            }
+                        }
+                        Log.d("Mainactivity", String.valueOf(result.getResultCode()));
+
+                    }
+                }
+
+        );
 
         ActivityResultLauncher<Intent> tshirtActivityResultLauncher = this.registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -39,16 +61,62 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                         Log.d("Mainactivity", String.valueOf(result.getResultCode()));
+
                     }
                 }
+
         );
+        ActivityResultLauncher<Intent> ManteauActivityResultLauncher = this.registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        switch (result.getResultCode()) {
+                            case RESULT_CANCELED:
+                                break;
+
+                            case RESULT_OK: {
+
+
+                            }
+                        }
+                        Log.d("Mainactivity", String.valueOf(result.getResultCode()));
+
+                    }
+                }
+
+        );
+
+
         ui.img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ManteauActivity.class);
+
+                ManteauActivityResultLauncher.launch(i);
+
+                overridePendingTransition(R.anim.slide_left, R.anim.slide_right);
+
+            }
+        });
+        ui.img2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Intent i = new Intent(MainActivity.this, TshirtActivity.class);
 
                 tshirtActivityResultLauncher.launch(i);
+                overridePendingTransition(R.anim.slide_left, R.anim.slide_right);
+
+            }
+
+        });
+        ui.img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AccessoireActivity.class);
+                accessoireActivityResultLauncher.launch(i);
+                overridePendingTransition(R.anim.slide_left, R.anim.slide_right);
 
             }
         });
